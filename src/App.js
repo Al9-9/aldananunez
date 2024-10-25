a8bcd446bc62c4711e67f7d6adb2c3dad4b63b0d
Al <aldananunez663@gmail.com>
2024-10-25 14:03:06 -0300

    Working on my pf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Portfolio from './components/Portfolio'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+           <ul className='links'>
+            <li className="link" >Inicio</li>
+            <li className="link" >Mi trabajo</li>
+            <li className="link" >Contacto</li>
+            <li className="link" >Sobre mí</li>
+          </ul></nav>
+         
       </header>
+      <main>
+<Portfolio/>
+      </main>
     </div>
   );
 }
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function Portfolio(){
+    return(
+        <div>
+            <h2>Mi Portfolio</h2> 
+            <img className='App-logo' src='/img.jpg' alt='my-img' style={{width: '300px', height: '300px'}}/>
+            <img className='Logo-port' src='/cat.webp' style={{width: '90px', height: '80px'}} />
+            <p>Welcome to my portfolio</p>
+             <p>Bienvenido a mi portfolio</p>
+            
+        </div> 
+      
+    )
+}
+
+
+
+export default Portfolio;
